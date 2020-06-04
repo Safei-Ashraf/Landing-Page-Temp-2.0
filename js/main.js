33a395d5cb126b4dff14939765298bd35c439e2a
Safei Ashraf <Safei.ashraf@gmail.com>
2020-06-04 22:18:52 +0200

    adding js random bg image for hero section

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//Select Landing Page:
+let landingPage = document.querySelector("#landing-page");
+
+///randomize bg img:
+function randomizeBackground() {
+  let randomizer = Math.floor(Math.random() * 5);
+  landingPage.style.backgroundImage = `url("/imgs/hero/${randomizer}.png")`;
+}
+//call random image function every 7s:
+setInterval(() => randomizeBackground(), 7000);
