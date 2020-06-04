1cd7d6e5f21c987d1b7246cf4db680991416d8bc
Safei Ashraf <Safei.ashraf@gmail.com>
2020-06-05 00:53:14 +0200

    adding toggle for settings box functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+//Settings box Section:
+//1-Click on Icon
+//2-Icon gets class "open" added
+//3-click again
+//4-Icon gets class "open" removed;
+let toggleBtn = document.querySelector("#toggle-btn");
+let settingsBox = document.querySelector("#settings-box");
+// console.log(toggleBtn);
+toggleBtn.addEventListener("click", () => {
+  settingsBox.classList.toggle("open");
+  console.log("clicked");
+});
+
 //Select Landing Page:
 let landingPage = document.querySelector("#landing-page");
 
