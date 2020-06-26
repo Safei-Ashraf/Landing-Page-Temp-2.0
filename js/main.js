7f98bc21b04c22eef2bd5101006b558450b565aa
Safei Ashraf <Safei.ashraf@gmail.com>
2020-06-26 20:18:44 +0200

    adding sound effects for mode switch

--- js/main.js
+++ js/main.js
@@ -27,12 +27,40 @@ toggleBtn.addEventListener("click", () => {
 
 ////////////Switch MODE REMAKE///////
 const chk = document.getElementById('chk');
-
+const lightSound = document.querySelector('#light-mode');
+const darkSound = document.querySelector('#dark-mode');
 chk.addEventListener('change', () => {
-	document.body.classList.toggle('light-mode');
+  document.body.classList.toggle('light-mode');
+  coolSounds();
 });
+//Sound Effects based on Mode:
 
+// function coolSounds (){
+//   chk.checked? lightSound.play(), da: darkSound.play();
+// }
+function coolSounds (){
+if(chk.checked == true)
+{
+  console.log('chkd is checked!')
+  darkSound.pause();
+  darkSound.currentTime = 0;
+  lightSound.play();
+
+}
+else
+{
+  console.log('chk is not checked');
+  lightSound.pause();
+  lightSound.currentTime = 0;
+  darkSound.play();
+}
+}
 
 particlesJS.load("particles-js", "assets/particles.json", function () {
   console.log("callback - particles.js config loaded");
 });
+
+
+
+
+
