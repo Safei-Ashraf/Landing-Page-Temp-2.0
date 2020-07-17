22f64f785e69675817948c931a2623cccb5460d0
Safei Ashraf <Safei.ashraf@gmail.com>
2020-07-18 01:30:57 +0200

    re-designing layout

--- js/landing.js
+++ js/landing.js
@@ -1,27 +1,28 @@
-//Select Landing Page:
-let landingPage = document.querySelector("#landing-page");
+// //Select Landing Page:
+// let landingPage = document.querySelector("#landing-page");
 
-///randomize bg img:
-function randomizeBackground() {
-  let randomizer = Math.floor(Math.random() * 5);
-  //console.log(`Randomizer value is ${randomizer}`);
-  landingPage.style.backgroundImage = `url("/imgs/hero/${randomizer}.png")`;
-}
-//call random image function every 7s:
-// setInterval(() => randomizeBackground(), 7000);
+// ///randomize bg img:
+// function randomizeBackground() {
+//   let randomizer = Math.floor(Math.random() * 5);
+//   //console.log(`Randomizer value is ${randomizer}`);
+//   landingPage.style.backgroundImage = `url("/imgs/hero/${randomizer}.png")`;
+// }
+// //call random image function every 7s:
+// // setInterval(() => randomizeBackground(), 7000);
 
-//Slide Navbar on Scroll:
-let navBar = document.querySelector("header.header-area");
+// //Slide Navbar on Scroll:
+// let navBar = document.querySelector("header.header-area");
 
-/* W3C BaseCode*/
-window.onscroll = function () {
-  scrollFunction();
-};
+// /* W3C BaseCode*/
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    navBar.style.top = "0";
-  } else {
-    navBar.style.top = "-100px";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+//     navBar.style.top = "0";
+//   } else {
+//     navBar.style.top = "-100px";
+//   }
+// }
+
--- js/main.js
+++ js/main.js
@@ -12,18 +12,18 @@ if (colorsTheme) {
 //2-Icon gets class "open" added
 //3-click again
 //4-Icon gets class "open" removed;
-let toggleBtn = document.querySelector("#toggle-btn");
-let settingsBox = document.querySelector("#settings-box");
-let toggleIcon = document.querySelector('#side-toggle');
+// let toggleBtn = document.querySelector("#toggle-btn");
+// let settingsBox = document.querySelector("#settings-box");
+// let toggleIcon = document.querySelector('#side-toggle');
 
-//Click event handler:
-toggleBtn.addEventListener("click", () => {
-  settingsBox.classList.toggle("open");
-  toggleIcon.classList.toggle('fa-chevron-circle-right');
-  //Switch Icon direction on Click:
-  toggleIcon.classList.toggle('fa-chevron-circle-left');
+// //Click event handler:
+// toggleBtn.addEventListener("click", () => {
+//   settingsBox.classList.toggle("open");
+//   toggleIcon.classList.toggle('fa-chevron-circle-right');
+//   //Switch Icon direction on Click:
+//   toggleIcon.classList.toggle('fa-chevron-circle-left');
 
-});
+// });
 
 ////////////Switch MODE REMAKE///////
 const chk = document.getElementById('chk');
