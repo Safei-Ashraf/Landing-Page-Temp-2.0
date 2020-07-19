529e8a6d36a0e0accbefb1e746b8241fbc6c8119
Safei Ashraf <Safei.ashraf@gmail.com>
2020-07-19 03:31:56 +0200

    adding new nav icon with menu

--- js/landing.js
+++ js/landing.js
@@ -1,28 +1,6 @@
-// //Select Landing Page:
-// let landingPage = document.querySelector("#landing-page");
-
-// ///randomize bg img:
-// function randomizeBackground() {
-//   let randomizer = Math.floor(Math.random() * 5);
-//   //console.log(`Randomizer value is ${randomizer}`);
-//   landingPage.style.backgroundImage = `url("/imgs/hero/${randomizer}.png")`;
-// }
-// //call random image function every 7s:
-// // setInterval(() => randomizeBackground(), 7000);
-
-// //Slide Navbar on Scroll:
-// let navBar = document.querySelector("header.header-area");
-
-// /* W3C BaseCode*/
-// window.onscroll = function () {
-//   scrollFunction();
-// };
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-//     navBar.style.top = "0";
-//   } else {
-//     navBar.style.top = "-100px";
-//   }
-// }
+/* Menu Icon Toggling Shape */
+const menuIcon = document.querySelector('.menu-container');
+const myNav = document.getElementById("myNav");
+menuIcon.addEventListener('click',()=>{menuIcon.classList.toggle("change");});
+menuIcon.addEventListener('click',()=>{myNav.classList.toggle("open");});
 
